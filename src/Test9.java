111182836db6d15e9168d44dd9f5edb7b65fc221
suchen <suchen_1992@163.com>
2018-07-03 21:09:23 +0800

    update test 9: Palindrome Number

---
+++ src/Test9.java
@@ -0,0 +1,17 @@
+public class Test9 {
+    // correct answer
+    public static boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return (x==rev || x==rev/10);
+    }
+
+    public static void main(String[] args) {
+        boolean b = Test9.isPalindrome(1000000001);
+        System.out.print(b);
+    }
+}
