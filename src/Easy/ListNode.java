29c890af9a19f39e557d2ff0319886634ce35e98
suchen <suchen_1992@163.com>
2018-08-08 22:35:58 +0800

    add Test19:Remove Nth Node From End of List

--- src/Easy/ListNode.java
+++ src/Easy/ListNode.java
@@ -1,7 +1,17 @@
 package Easy;
 // Test21 create list
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+
+    public static ListNode construct(int[] nums) {
+        ListNode l = new ListNode(nums[0]);
+        ListNode p = l;
+        for (int i = 1; i < nums.length; i++) {
+            p.next = new ListNode(nums[i]);
+            p = p.next;
+        }
+        return l;
+    }
 }
